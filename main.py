733007b1c5eb52b417bebcb9d1c22ca301c1d253
Jack Lin <jacklin@cybersoft4u.com>
2022-12-14 13:22:33 +0800

    Update

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import cv2
 def main():
     img1 = cv2.imread("Dog_Strong.jpg")
     img2 = cv2.imread("Dog_Weak.jpg")
+    window_name = "Blending"
 
     def change_trackbar(val):
         alpha = float(val / 255)
         blend_img = cv2.addWeighted(img2, alpha, img1, 1.0 - alpha, 0.0)
-        cv2.imshow("Blending", blend_img)
+        cv2.imshow(window_name, blend_img)
     
-    cv2.namedWindow("Blending")
-    cv2.createTrackbar("trackbar", "Blending", 0, 255, change_trackbar)
-    cv2.imshow("Blending", img1)
+    cv2.namedWindow(window_name)
+    cv2.createTrackbar("trackbar", window_name, 0, 255, change_trackbar)
+    cv2.imshow(window_name, img1)
     while True:
         if cv2.waitKey(100) == 27:  # ESC
-            cv2.destroyWindow("Image")
+            cv2.destroyWindow(window_name)
             break
 
 
--- main2.py
+++ main2.py
@@ -4,39 +4,40 @@ import cv2
 def main():
     img1 = cv2.imread("Dog_Strong.jpg")
     img2 = cv2.imread("Dog_Weak.jpg")
+    window_name = "Blending"
     alpha = 0.0
     beta = 0.0
     gamma = 0.0
 
     def alpha_trackbar(val):
         alpha = float(val / 255)
-        beta = float(cv2.getTrackbarPos("beta", "Blending") / 255)
-        gamma = float(cv2.getTrackbarPos("gamma", "Blending"))
+        beta = float(cv2.getTrackbarPos("beta", window_name) / 255)
+        gamma = float(cv2.getTrackbarPos("gamma", window_name))
         blend_img = cv2.addWeighted(img2, alpha, img1, beta, gamma)
-        cv2.imshow("Blending", blend_img)
+        cv2.imshow(window_name, blend_img)
 
     def beta_trackbar(val):
         beta = float(val / 255)
-        alpha = float(cv2.getTrackbarPos("alpha", "Blending") / 255)
-        gamma = float(cv2.getTrackbarPos("gamma", "Blending"))
+        alpha = float(cv2.getTrackbarPos("alpha", window_name) / 255)
+        gamma = float(cv2.getTrackbarPos("gamma", window_name))
         blend_img = cv2.addWeighted(img2, alpha, img1, beta, gamma)
-        cv2.imshow("Blending", blend_img)
+        cv2.imshow(window_name, blend_img)
 
     def gamma_trackbar(val):
         gamma = val
-        alpha = float(cv2.getTrackbarPos("alpha", "Blending") / 255)
-        beta = float(cv2.getTrackbarPos("beta", "Blending") / 255)
+        alpha = float(cv2.getTrackbarPos("alpha", window_name) / 255)
+        beta = float(cv2.getTrackbarPos("beta", window_name) / 255)
         blend_img = cv2.addWeighted(img2, alpha, img1, beta, gamma)
-        cv2.imshow("Blending", blend_img)
+        cv2.imshow(window_name, blend_img)
 
-    cv2.namedWindow("Blending")
-    cv2.createTrackbar("alpha", "Blending", 0, 255, alpha_trackbar)
-    cv2.createTrackbar("beta", "Blending", 0, 255, beta_trackbar)
-    cv2.createTrackbar("gamma", "Blending", 0, 255, gamma_trackbar)
-    cv2.imshow("Blending", img1)
+    cv2.namedWindow(window_name)
+    cv2.createTrackbar("alpha", window_name, 0, 255, alpha_trackbar)
+    cv2.createTrackbar("beta", window_name, 0, 255, beta_trackbar)
+    cv2.createTrackbar("gamma", window_name, 0, 255, gamma_trackbar)
+    cv2.imshow(window_name, img1)
     while True:
         if cv2.waitKey(100) == 27:  # ESC
-            cv2.destroyWindow("Image")
+            cv2.destroyWindow(window_name)
             break
 
 
